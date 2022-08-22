a45dc98bf70a6efb9e19f0bbc28acd7d2ec683f4
l0rdluc1f3r <luc1f3rrr@protonmail.ch>
2022-08-22 20:27:41 +0530

    initial commit

--- src/com/lucifer/DataStructures/Main.java
+++ src/com/lucifer/DataStructures/Main.java
@@ -4,15 +4,29 @@ import java.util.LinkedList;
 
 public class Main {
     public static void main(String[] args) {
-        LL list = new LL();
+        // for single linked list
+//        LL list = new LL();
+//        list.insertFirst(2);
+//        list.insertFirst(5);
+//        list.insertFirst(9);
+//        list.insertFirst(12);
+//        list.insertFirst(17);
+//        list.insertLast(22);
+//        list.insert(33,3);
+//        System.out.println(list.deleteFirst());
+//        list.display();
+//        System.out.println(list.deleteLast());
+//        list.display();
+//        System.out.println(list.delete(3));
+//        list.display();
+
+        DLL list = new DLL();
         list.insertFirst(2);
         list.insertFirst(5);
         list.insertFirst(9);
         list.insertFirst(12);
         list.insertFirst(17);
-        list.insertLast(22);
-        list.insert(33,3);
-        System.out.println(list.deleteFirst());
+
         list.display();
     }
 }
